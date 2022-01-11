cd43dd44213a3e1310e172dbc4726955e7dc94d7
Cameron Zipp <Zipp.Cameron@student.greenriver.edu>
2022-01-11 17:38:18 -0500

    Correcting php logic flaw

--- functions.php
+++ functions.php
@@ -10,6 +10,10 @@
         echo max($numbers);
     }
 
-    function removeDups($numbers) {
-        array_unique($numbers);
+    function removeDups(array $numbers) {
+        $numbers = array_unique($numbers, SORT_NUMERIC);
+
+        foreach ($numbers as $value) {
+            echo "<p>" . $value . "</p>";
+        }
     }
