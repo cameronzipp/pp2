e568811515b28999e81d157c663cd6e6ca2ed170
Cameron Zipp <Zipp.Cameron@student.greenriver.edu>
2022-01-11 17:07:40 -0500

    adding index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+    <h2>Pair Program 2</h2>
+</body>
+</html>
